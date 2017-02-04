c0a285359810bef8c73107c4a1452369778ace56
Austin <austing8012@gmail.com>
2017-02-04 12:34:28 -0800

    added the password and flag so they can complete the challenge

--- overflow/main.cpp
+++ overflow/main.cpp
@@ -7,23 +7,18 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-        int pass = 0;
-       char mybuffer[5];
-       cout<<"please enter the password: ";
-       // a prompt how to execute the program...
-       gets(mybuffer);
-        if(strcmp(mybuffer, "Pass")){
-            cout<<endl<<"Access Denied" << endl;
-        }
-        else {
-            cout<<endl <<"Access Granted"<<endl;
-            cout<<"Flage = nsdlklndfsnflnnfnlaf" <<endl;
-            pass = 1;
-        }
-
-        if(pass){
-            cout<<endl<<"root Privileges"<<endl;
-        }
-
-
+   int pass = 0;
+   char mybuffer[30];
+   cout<<"please enter the password: ";
+   gets(mybuffer);
+   if(strcmp(mybuffer, "b3tY0ucanTgu3ssM3333")){
+        cout<<endl<<"Access Denied" << endl;
+    } else {
+        cout<<endl <<"Access Granted"<<endl;
+        cout<<"Flage = 133" <<endl;
+        pass = 1;
+    }
+    if(pass){
+        cout<<endl<<"root Privileges Flag = ~1337bufferOv3fl0wz!"<<endl;
+    }
 }
