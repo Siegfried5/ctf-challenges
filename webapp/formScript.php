d185d7fd9d97be0d9ac6b4448c347ed5ecd9ee7c
Rudolph Ponce <rudolph@TagPay.com>
2017-03-14 14:37:56 -0700

    mod teh web/formscript.php

--- webapp/formScript.php
+++ webapp/formScript.php
@@ -1,6 +1,5 @@
 <?php
 
-
 $customerName =$_POST['customerName'];
 $email = $_POST['email'];
 $message = $_POST['message'];
@@ -10,20 +9,60 @@ echo "email:".$email."<br/>";
 echo "message:".$message."<br/>";
 
 
+
 // challenge level 1
-eval($customerName);
-eval($email);
-eval($message);
+// $strip=strrpos($customerName, ";");
+// if ($strip === false  ){
+//
+// } else {
+//   echo "bad";
+//   eval($customerName);
+// }
+
+
 
 // challenge level 2
+// $strip=strrpos($email, ";");
+// if ($strip === false  ){
+//
+// }
+//
+// else {
+//
+//   if ($strip !== false ){
+//     echo "bad";
+//     $email = "";
+//   }else{
+//     eval(urldecode($email));
+//   }
+// }
+
+
+//challenge level 3
+// eval($message);
+
+// challenge level 4
 // if ($message == "phpinfo();"){
 //   $message = "";
 // }
 // eval(urldecode($message));
 
 
-// challenge level 3
-
+// challenge 5
+// if ($message == "phpinfo()%3B"){
+//   $message = "";
+// }
+//
+// $find_sem=strrpos($message, ";");
+// echo $find_sem;
+//
+// if ($find_sem === false){
+//   echo "good";
+//   $message = "";
+// } else {
+//   $message = "";
+// }
+//
 // eval($message);
-
+//
  ?>
